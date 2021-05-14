07424ab1106b060deab494a3d443d8e9fbd865e4
saraperse <srh.rouhani@gmail.com>
2021-05-14 10:29:05 +0200

    boucle tant que en js

---
+++ exo1.js
@@ -0,0 +1,10 @@
+var readline = require("readline-sync");
+
+
+var message = readline.question("saisir coucou : ");
+while ( message !== "coucou"){
+ 
+var message = readline.question("saisir cououc : !");   
+}
+
+
